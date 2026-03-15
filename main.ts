5d21664c8ac86e222824611f8948a51440108e33
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-03-15 12:28:52 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,69 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    for (let index = 0; index < 4; index++) {
+        callibot2.write_rgbled(0x00ff00)
+        callibot2.write_motoren_prozent(60, 60)
+        callibot2.encoder_wait_cm(20)
+        callibot2.write_rgbled(0xa300ff)
+        callibot2.write_motoren_prozent(60, -60)
+        callibot2.encoder_wait_grad(90)
+    }
+    callibot2.write_motoren_prozent(-100, 100)
+    callibot2.encoder_wait_grad(360)
+    callibot2.write_rgbled(0x000000)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (go == 0) {
+        go = 1
+        basic.setLedColor(0xffffff)
+    } else {
+        go = 0
+        callibot2.write_motor_prozent(callibot2.eMotor.beide, 0)
+        basic.setLedColor(0xff0000)
+    }
+})
+function spur_verloren () {
+    basic.setLedColor(0xffff00)
+    callibot2.write_motoren_prozent(25, -25)
+    while (callibot2.get_inputs(callibot2.eINPUTS.sp2l) || callibot2.get_inputs(callibot2.eINPUTS.sp1r)) {
+        basic.pause(100)
+        callibot2.read_inputs()
+    }
+    callibot2.write_motor_prozent(callibot2.eMotor.beide, 0)
+    basic.setLedColor(0x00ff00)
+    go = 2
+}
+let go = 0
+callibot2.reset_outputs()
+go = 0
+basic.showNumber(callibot2.read_power())
+basic.showString(callibot2.read_typ())
+loops.everyInterval(500, function () {
+    callibot2.write_led(callibot2.eLed.poweron, true, true)
+})
+basic.forever(function () {
+    if (go == 1) {
+        callibot2.comment("1 bis zur Linie geradeaus fahren")
+        callibot2.write_motor_prozent(callibot2.eMotor.beide, 65)
+        callibot2.read_inputs()
+        if (!(callibot2.get_inputs(callibot2.eINPUTS.sp2l)) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
+            callibot2.comment("beide schwarz, Linie gefunden -> 2")
+            spur_verloren()
+        }
+    } else if (go == 2) {
+        callibot2.comment("2 Spur folgen")
+        callibot2.read_inputs()
+        if (!(callibot2.get_inputs(callibot2.eINPUTS.sp2l)) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
+            callibot2.comment("beide schwarz")
+            callibot2.write_motor_prozent(callibot2.eMotor.beide, 50)
+        } else if (callibot2.get_inputs(callibot2.eINPUTS.sp2l) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
+            callibot2.comment("nach rechts lenken")
+            callibot2.write_motoren_prozent(50, -50)
+        } else if (!(callibot2.get_inputs(callibot2.eINPUTS.sp2l)) && callibot2.get_inputs(callibot2.eINPUTS.sp1r)) {
+            callibot2.comment("nach links lenken")
+            callibot2.write_motoren_prozent(-50, 50)
+        } else if (callibot2.get_inputs(callibot2.eINPUTS.sp2l) && callibot2.get_inputs(callibot2.eINPUTS.sp1r)) {
+            callibot2.comment("beide hell: Spur verloren")
+            spur_verloren()
+        }
+    }
+})
