0b6a2db60e0ed22e9dd704aaf7dfbc931131b0eb
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-03-15 15:45:30 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    callibot2.comment("Ultraschall Abstand Sensor")
     callibot2.write_motor_prozent(callibot2.eMotor.beide, 80)
     callibot2.wait_us(20)
+    callibot2.write_rgbled(0xa300ff)
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    callibot2.comment("nur Calli:bot 2E mit Encoder")
     for (let index = 0; index < 4; index++) {
         callibot2.write_rgbled(0x00ff00)
         callibot2.write_motoren_prozent(60, 60)
@@ -16,32 +19,29 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     callibot2.write_rgbled(0x000000)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    if (go == 0) {
-        go = 1
-        basic.setLedColor(0xffff00)
+    if (!(go)) {
+        callibot2.comment("bis zur Linie geradeaus fahren")
+        callibot2.write_motor_prozent(callibot2.eMotor.beide, 50)
+        callibot2.wait_spursensor()
+        go = true
+        basic.setLedColor(0x00ff00)
     } else {
-        go = 0
-        basic.setLedColor(0xff0000)
+        callibot2.comment("B: STOP")
         callibot2.write_motor_prozent(callibot2.eMotor.beide, 0)
+        go = false
+        basic.setLedColor(0xff0000)
     }
 })
-let go = 0
+let go = false
 callibot2.reset_outputs()
-go = 0
 basic.showNumber(callibot2.read_power())
 basic.showString(callibot2.read_typ())
 basic.forever(function () {
-    if (go == 1) {
-        callibot2.comment("1 bis zur Linie geradeaus fahren")
-        callibot2.write_motor_prozent(callibot2.eMotor.beide, 50)
-        callibot2.wait_spursensor()
-        go = 2
-        basic.setLedColor(0x00ff00)
-    } else if (go == 2) {
-        callibot2.comment("2 Spur folgen")
+    if (go) {
+        callibot2.comment("Spur folgen")
         callibot2.read_inputs()
         if (!(callibot2.get_inputs(callibot2.eINPUTS.sp2l)) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
-            callibot2.comment("beide schwarz")
+            callibot2.comment("beide Sensoren dunkel- auf der Linie")
             callibot2.write_motor_prozent(callibot2.eMotor.beide, 100)
         } else if (callibot2.get_inputs(callibot2.eINPUTS.sp2l) && !(callibot2.get_inputs(callibot2.eINPUTS.sp1r))) {
             callibot2.comment("nach rechts lenken")
